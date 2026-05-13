e2fa1e819a72ae5b81a68d5cfb18a998765aabb0
rlaeodn1015-oss <rlaeodn1015@gmail.com>
2026-05-13 17:02:22 +0900

    Add proData.js with product list and images

---
+++ proData.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images";
+
+const PRO_LIST = [
+    { id: 1, name: "프로1", src: `${BASE_URL}/KakaoTalk_20260327_165459882.png` },
+    { id: 2, name: "JYH", src: `${BASE_URL}/JYH.jpg` },
+    { id: 3, name: "프로2", src: `${BASE_URL}/KakaoTalk_20260309_200933587.jpg` },
+    { id: 4, name: "SJH", src: `${BASE_URL}/SJH.png` },
+    { id: 5, name: "HJY", src: `${BASE_URL}/HJY.png` },
+    { id: 6, name: "YHC", src: `${BASE_URL}/YHC.png` },
+    { id: 7, name: "JDY", src: `${BASE_URL}/JDY.png` },
+    { id: 8, name: "KNY", src: `${BASE_URL}/KNY.jpg` },
+    { id: 9, name: "LWY", src: `${BASE_URL}/lwy.webp` },
+    { id: 10, name: "YMH", src: `${BASE_URL}/YMH.png` },
+    { id: 11, name: "KDY", src: `${BASE_URL}/kdy.webp` },
+    { id: 12, name: "KTY", src: `${BASE_URL}/KTY.png` },
+    { id: 13, name: "LSJ", src: `${BASE_URL}/LSJ.png` },
+    { id: 14, name: "KDW", src: `${BASE_URL}/KDW.jpg` },
+    { id: 15, name: "LGH", src: `${BASE_URL}/LGH.webp` }
+];
